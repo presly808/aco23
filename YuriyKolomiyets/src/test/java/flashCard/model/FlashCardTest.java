9810f4e197b008da193c1bd6713f4fb60e10f4d9
yuriykolomiyets <juriy121@gmail.com>
2018-01-15 18:03:47 +0200

    FlashCard - YuriyKolomiyets

---
+++ YuriyKolomiyets/src/test/java/flashCard/model/FlashCardTest.java
@@ -0,0 +1,56 @@
+package flashCard.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FlashCardTest {
+
+    @Test
+    public void getId() {
+
+        FlashCard flashCard = new FlashCard(1, "A", "b");
+        Assert.assertEquals(1, flashCard.getId());
+    }
+
+    @Test
+    public void setId() {
+        FlashCard flashCard = new FlashCard();
+        flashCard.setId(1);
+        Assert.assertEquals(1, flashCard.getId());
+    }
+
+    @Test
+    public void getFront() {
+        FlashCard flashCard = new FlashCard(1, "A", "b");
+        Assert.assertEquals("A", flashCard.getFront());
+    }
+
+    @Test
+    public void setFront() {
+        FlashCard flashCard = new FlashCard();
+        flashCard.setFront("A");
+        Assert.assertEquals("A", flashCard.getFront());
+    }
+
+    @Test
+    public void getBack() {
+        FlashCard flashCard = new FlashCard(1, "A", "b");
+        Assert.assertEquals("b", flashCard.getBack());
+    }
+
+    @Test
+    public void setBack() {
+        FlashCard flashCard = new FlashCard();
+        flashCard.setBack("b");
+        Assert.assertEquals("b", flashCard.getBack());
+    }
+
+    /*@Test
+    public String toString() {
+        FlashCard flashCard = new FlashCard(1, "A", "b");
+        Assert.assertEquals("FlashCard id 1, front: A, back: b", flashCard.toString());
+        return  flashCard.toString();
+    }*/
+}
