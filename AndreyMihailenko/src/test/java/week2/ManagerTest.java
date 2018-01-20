7a9bdc9ac579a18ad098eed27061a1d40e0ee9f6
Mihal <miandmih@gmail.com>
2018-01-20 10:34:40 +0200

    Week2 - ManagerTask - AndreyMihailenko

---
+++ AndreyMihailenko/src/test/java/week2/ManagerTest.java
@@ -0,0 +1,56 @@
+package week2;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by serhii on 19.01.18.
+ */
+public class ManagerTest {
+    @Test
+    public void calculateSalary() throws Exception {
+        Manager manager = new Manager("Oleg", 3000);
+        manager.addSubworker(new Employee("Ivan", 2000));
+        manager.addSubworker(new Employee("Valera", 2000));
+        manager.addSubworker(new Employee("Olena", 2000));
+
+        Assert.assertThat(manager.calculateSalary(), CoreMatchers.equalTo(3300));
+
+    }    @Test
+    public void calculateSalary1() throws Exception {
+        Manager manager = new Manager("Oleg", 3000);
+        Manager manager1 = new Manager("Andrey", 4000);
+
+        manager.addSubworker(new Employee("Ivan", 2000));
+        manager.addSubworker(new Employee("Valera", 2000));
+
+        manager.addSubworker(manager1);
+
+
+        int actual = 3000 + (2000 / 20 + 2000 / 20) + 4000 / 20;
+
+        Assert.assertThat(manager.calculateSalary(), CoreMatchers.equalTo(actual));
+
+    }
+
+    @Test
+    public void addSubworker() throws Exception {
+        Manager manager = new Manager("Oleg", 3000);
+        Assert.assertThat(manager.addSubworker(new Employee("Ivan", 2000)),
+                CoreMatchers.equalTo(true));
+    }
+
+    @Test
+    public void getSubworkers() throws Exception {
+        Manager manager = new Manager("Oleg", 3000);
+        manager.addSubworker(new Employee("Ivan", 2000));
+        manager.addSubworker(new Employee("Valera", 2000));
+        manager.addSubworker(new Employee("Petro", 2000));
+
+        Assert.assertThat(manager.getSubworkers().size(), CoreMatchers.equalTo(3));
+    }
+
+}
