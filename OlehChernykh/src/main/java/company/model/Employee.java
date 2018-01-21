5b3bbd95b8179ddd7a7e214e26eee067d4a46225
ochernykh <0595887@gmail.com>
2018-01-21 11:20:05 +0200

    week1-flashcard-Oleh Chernykh

---
+++ OlehChernykh/src/main/java/company/model/Employee.java
@@ -0,0 +1,29 @@
+package company.model;
+
+
+import company.utils.MyUtils;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.id = MyUtils.genId();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+
+}
---
+++ OlehChernykh/src/main/java/company/model/Manager.java
@@ -0,0 +1,27 @@
+package company.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * You can add any additional classes to make structure more flexible and scalable
+ */
+public class Manager extends Employee {
+
+    private ArrayList<Employee> employees;
+
+    public Manager(String name, int salary) {
+        super(name,salary);
+
+    }
+
+    public boolean addSubworker(Employee employee){
+        return employees.add(employee);
+    }
+
+    public List<Employee> getSubworkers(){
+        return employees;
+    }
+
+
+}
