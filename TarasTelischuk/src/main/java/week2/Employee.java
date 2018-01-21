c48d04c67f142708c5fb41910c614dccffc3a79e
telishuk <telishuk.t@gmail.com>
2018-01-21 10:57:23 +0200

    <week2>-<TarasTelischuk>

--- TarasTelischuk/src/main/java/week2/Employee.java
+++ TarasTelischuk/src/main/java/week2/Employee.java
@@ -7,27 +7,10 @@ public class Employee implements Worker {
     private int salary;
 
     public Employee() {
-
     }
 
     public Employee(String name, int salary) {
         this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
         this.salary = salary;
     }
 
