3f2806e6335a07527bdfef019303efd590d4e04a
yuriykolomiyets <juriy121@gmail.com>
2018-01-24 11:36:14 +0200

    Week2 - Company - YuriyKolomiyets

--- YuriyKolomiyets/src/main/java/company/utils/MyUtils.java
+++ YuriyKolomiyets/src/main/java/company/utils/MyUtils.java
@@ -1,10 +1,5 @@
 package company.utils;
 
-import company.db.AppDb;
-
-/**
- * Created by serhii on 20.01.18.
- */
 public class MyUtils {
     public static int genId() {
         return (int)(Math.random()*100);
