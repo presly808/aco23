4b50cab3fe08852abd6bd7084b7e82a0e76cacf4
yuriykolomiyets <juriy121@gmail.com>
2018-01-24 11:30:56 +0200

    Week2 - Company - YuriyKolomiyets

--- YuriyKolomiyets/src/test/java/company/model/EmployeeTest.java
+++ YuriyKolomiyets/src/test/java/company/model/EmployeeTest.java
@@ -1,8 +1,5 @@
 package company.model;
-
-import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class EmployeeTest {
@@ -10,32 +7,33 @@ public class EmployeeTest {
     @Test
     public void getName() {
         Employee employee = new Employee("Oleg", 300);
+        assertEquals("Oleg", employee.getName());
     }
 
     @Test
     public void setName() {
         Employee employee = new Employee();
         employee.setName("Oleg");
-        Assert.assertEquals("Oleg", employee.getName());
+        assertEquals("Oleg", employee.getName());
     }
 
     @Test
     public void getSalary() {
         Employee employee = new Employee("Oleg", 300);
-        Assert.assertEquals(300, employee.getSalary());
+        assertEquals(300, employee.getSalary());
     }
 
     @Test
     public void setSalary() {
         Employee employee = new Employee();
         employee.setSalary(100);
-        Assert.assertEquals(100, employee.getSalary());
+        assertEquals(100, employee.getSalary());
     }
 
     @Test
     public void calculateSalary() {
         Employee employee = new Employee();
         employee.setSalary(100);
-        Assert.assertEquals(100, employee.calculateSalary());
+        assertEquals(100, employee.calculateSalary());
     }
 }
