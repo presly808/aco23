228250d1a9d34fa9ab8131d6f6f8ad2719137216
telishuk <telishuk.t@gmail.com>
2018-02-02 17:18:17 +0200

    <week3><TarasTelischuk>

--- functional-tests/src/main/java/company/utils/factory/MainFactory.java
+++ functional-tests/src/main/java/company/utils/factory/MainFactory.java
@@ -4,12 +4,12 @@ import company.controller.MainController;
 import company.controller.MainControllerImpl;
 import company.controller.MainControllerTime;
 import company.db.AppDb;
-import company.model.Employee;
+/*import company.model.Employee;
 import company.notifier.MyListener;
 import company.utils.filtering.EmployeePredicate;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.List;*/
 
 /**
  * Created by serhii on 27.01.18.
