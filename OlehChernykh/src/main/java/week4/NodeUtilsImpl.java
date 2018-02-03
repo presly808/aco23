74b91f55a81828e04d052033147ebae8f266b72f
ochernykh <0595887@gmail.com>
2018-02-03 11:35:35 +0200

    week3-patterns-Oleh Chernykh

---
+++ OlehChernykh/src/main/java/week4/NodeUtilsImpl.java
@@ -0,0 +1,108 @@
+package week4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+public class NodeUtilsImpl implements NodeUtils {
+
+    @Override
+    public void addToTail(Node first, Object val) {
+        while (first.next!=null)first = first.next;
+        first.next = new Node(null,val);
+    }
+
+    @Override
+    public Node addToHead(Node chain, Object val) {
+        Node head = new Node(chain,val);
+        return head;
+    }
+
+    @Override
+    public String toString(Node chain) {
+        String res = "";
+        Node iter = chain;
+        while (iter != null) {
+            res += iter.value + "->";
+            iter = iter.next;
+        }
+        return res;
+    }
+
+    @Override
+    public Node createNode(Object... mas) {
+        Node head = new Node(null, mas[0]);
+        Node last = head;
+        for (int i = 1; i <mas.length; i++) {
+            last.next = new Node(null,mas[i]);
+            last = last.next;
+        }
+        return head;
+    }
+
+    @Override
+    public Node createNodeR(Object... mas) {
+        return null;
+    }
+
+    @Override
+    public int count(Node chain) {
+        int count = 0;
+        while (chain != null)
+        {
+            count++;
+            chain = chain.next;
+        }
+        return count;
+    }
+
+    @Override
+    public Node remove(Node chain, Object val) {
+        chain.value = chain.next.value;
+        chain.next = chain.next.next;
+        return chain;
+    }
+
+    @Override
+    public Object[] toArray(Node chain) {
+        List<Object> objects = new ArrayList<>();
+        while (chain.next != null) {
+            objects.add(chain.value);
+            chain = chain.next;
+        }
+        Object[] obj = objects.toArray();
+
+        return obj;
+    }
+
+    @Override
+    public Node reverse(Node curr) {
+        Node prev = null;
+        Node current = curr;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        curr = prev;
+        return curr;
+    }
+
+
+    @Override
+    public Node reverse(Node curr, Node next, Node prev) {
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr = prev;
+        return curr;
+    }
+}
---
+++ OlehChernykh/src/test/java/week4/recursion/BacktrackingRecTest.java
@@ -0,0 +1,49 @@
+package week4.recursion;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+
+/*
+public class BacktrackingRecTest {
+    @Test
+    public void canFindSum() throws Exception {
+        boolean res = BacktrackingRec.canFindSum(new int[]{1,5,10,20,9,7},34);
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void findChainIfPossible() throws Exception {
+        int[] res = BacktrackingRec.findChainIfPossible(new int[]{1,5,10,20,9,7},34);
+        Assert.assertArrayEquals(new int[]{5,20,9},res);
+
+    }
+
+    @Test
+    public void findChainIfPossibleNeg() throws Exception {
+        int[] res = BacktrackingRec.findChainIfPossible(new int[]{1,5,10,20,9,7},115);
+        Assert.assertArrayEquals(new int[]{},res);
+
+    }
+    @Test
+    public void findChainIfPossibleNeg2() throws Exception {
+        int[] res = BacktrackingRec.findChainIfPossible(new int[]{1,5,10,20,9,7},4);
+        Assert.assertArrayEquals(new int[]{},res);
+
+    }
+
+
+    @Test
+    public void allPossibleCombination() throws Exception {
+
+
+    }
+
+}
+
+*/
