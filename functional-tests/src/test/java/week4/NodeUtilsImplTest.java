93b3180d0a3e3a62ce9a2550f86bc4a2d4a15720
presly808 <presly808@gmail.com>
2018-02-03 10:00:48 +0200

    fixed reverse

--- functional-tests/src/test/java/week4/NodeUtilsImplTest.java
+++ functional-tests/src/test/java/week4/NodeUtilsImplTest.java
@@ -101,8 +101,8 @@ public class NodeUtilsImplTest {
     public void reverse() throws Exception {
         Node node = new Node(new Node(new Node(null,3), 4), 5);
         Node newHead = nodeUtils.reverse(node);
-        Assert.assertThat(newHead.value, CoreMatchers.equalTo(5));
-        Assert.assertThat(newHead.next.next.value, CoreMatchers.equalTo(3));
+        Assert.assertThat(newHead.value, CoreMatchers.equalTo(3));
+        Assert.assertThat(newHead.next.next.value, CoreMatchers.equalTo(5));
 
     }
 
