23f2dfe9d1cd708658c748c51ebba18ae3a72ef9
Valentina <allthemore@yandex.ru>
2018-02-03 09:45:11 +0200

    Init week4 task

---
+++ ValentynaMusiiaka/src/test/java/week4/NodeUtilsImplTest.java
@@ -0,0 +1,47 @@
+package week4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+public class NodeUtilsImplTest {
+    @Test
+    public void addToTail() throws Exception {
+    }
+
+    @Test
+    public void addToHead() throws Exception {
+    }
+
+    @Test
+    public void testToString() throws Exception {
+    }
+
+    @Test
+    public void createNode() throws Exception {
+    }
+
+    @Test
+    public void createNodeR() throws Exception {
+    }
+
+    @Test
+    public void count() throws Exception {
+    }
+
+    @Test
+    public void remove() throws Exception {
+    }
+
+    @Test
+    public void toArray() throws Exception {
+    }
+
+    @Test
+    public void reverse() throws Exception {
+    }
+
+}
