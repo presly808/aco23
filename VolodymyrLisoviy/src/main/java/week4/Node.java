bdcd5d89d23aa781566b413e95e8e2befa6a92bc
Foresstt <les.volodumur@gmail.com>
2018-02-03 11:08:20 +0200

    Week4 - node_task - VolodumurLisoviy - fix test coverage

--- VolodymyrLisoviy/src/main/java/week4/Node.java
+++ VolodymyrLisoviy/src/main/java/week4/Node.java
@@ -16,20 +16,6 @@ public class Node {
         this.value = value;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Node node = (Node) o;
-
-        return value.equals(node.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
 
     @Override
     public String toString() {
