3516fdff872a0ec81d5402db52563dcd08b6b604
presly808 <presly808@gmail.com>
2018-02-03 09:02:08 +0200

    added NodeUtils task

---
+++ functional-tests/src/main/java/week4/NodeUtilsImpl.java
@@ -0,0 +1,57 @@
+package week4;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+public class NodeUtilsImpl implements NodeUtils {
+
+    @Override
+    public void addToTail(Node first, Object val) {
+
+    }
+
+    @Override
+    public Node addToHead(Node chain, Object val) {
+        return null;
+    }
+
+    @Override
+    public String toString(Node chain) {
+        return null;
+    }
+
+    @Override
+    public Node createNode(Object... mas) {
+        return null;
+    }
+
+    @Override
+    public Node createNodeR(Object... mas) {
+        return null;
+    }
+
+    @Override
+    public int count(Node chain) {
+        return 0;
+    }
+
+    @Override
+    public Node remove(Node chain, Object val) {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray(Node chain) {
+        return new Object[0];
+    }
+
+    @Override
+    public Node reverse(Node curr) {
+        return null;
+    }
+
+    @Override
+    public Node reverse(Node curr, Node next, Node prev) {
+        return null;
+    }
+}
