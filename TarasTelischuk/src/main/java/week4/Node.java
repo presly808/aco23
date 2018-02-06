5e442c5ce2fa0e2105a375334f952cb6b7e1e547
telishuk <telishuk.t@gmail.com>
2018-02-06 20:58:27 +0200

    <week4><Telischuk>

--- TarasTelischuk/src/main/java/week4/Node.java
+++ TarasTelischuk/src/main/java/week4/Node.java
@@ -16,20 +16,4 @@ public class Node {
         this.value = value;
     }
 
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public void setValue(Object value) {
-        this.value = value;
-    }
 }
