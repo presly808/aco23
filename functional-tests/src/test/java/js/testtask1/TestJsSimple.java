4295fe0a43348894895202e27a4625b0e4642a65
presly808 <presly808@gmail.com>
2018-02-10 11:31:55 +0200

    added tests

--- functional-tests/src/test/java/js/testtask1/TestJsSimple.java
+++ functional-tests/src/test/java/js/testtask1/TestJsSimple.java
@@ -15,11 +15,20 @@ public class TestJsSimple {
 
     @Test
     public void testCheck() throws FileNotFoundException, ScriptException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
 
         ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("nashorn");
 //        Object res = scriptEngine.eval(new FileReader(TestJsSimple.class.getResource("task1.js").getFile()));
         Object res = scriptEngine.eval(
                 new FileReader("/home/serhii/dev/projects/aco23/functional-tests/src/main/java/js/testtask1/task1.js"));
+
+
+        String s = baos.toString();
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        System.out.println("My out " + s);
+
 
     }
 }
