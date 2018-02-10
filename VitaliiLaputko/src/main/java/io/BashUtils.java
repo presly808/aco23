4e550e205fc71da34e75659f917f2ad59abb7e2d
Vitalii Laputko <vitalii.l@sbtech.com>
2018-02-10 11:39:46 +0200

    first parth

--- VitaliiLaputko/src/main/java/io/BashUtils.java
+++ VitaliiLaputko/src/main/java/io/BashUtils.java
@@ -1,7 +1,11 @@
 package io;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,38 +13,103 @@ import java.util.Map;
  * Created by serhii on 10.02.18.
  */
 public class BashUtils {
-
     public static String cat(String path) throws FileNotFoundException {
-        return null;
+        Reader reader = new FileReader(path);
+        StringBuilder sb = new StringBuilder();
+        char[] buff = new char[100000];
+
+        try {
+            while (reader.read(buff) != -1)
+                sb.append(buff);
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return sb.toString();
     }
 
     public static boolean writeInto(String path, String src, boolean append)
             throws IOException {
-        return false;
+        Writer writer = new BufferedWriter(new FileWriter(path, append));
+        boolean flag = true;
+
+        try {
+            writer.write(src);
+            writer.flush();
+        } catch (IOException e) {
+            flag = false;
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                flag = false;
+                e.printStackTrace();
+            }
+        }
+
+        return flag;
     }
 
     public static List<String> ls(String path) throws FileNotFoundException {
-        return null;
+        File folder = new File(
+                new File(path).toURI().relativize(
+                        new File(path).toURI()).getPath());
+        List<String> list = new ArrayList<>();
+
+        for (File listOfFile : folder.listFiles()) {
+            if (listOfFile.isFile())
+                list.add(listOfFile.getName());
+        }
+
+        return list;
     }
 
     public static boolean copy(String src, String dest) throws Exception {
-        return false;
+        System.out.println(new File(src).getAbsolutePath());
+
+        try (InputStream input = new FileInputStream(new File(src).getAbsolutePath());
+             OutputStream output = new FileOutputStream(new File(dest).getAbsolutePath())) {
+            byte[] buf = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buf)) > 0) {
+                output.write(buf, 0, bytesRead);
+            }
+        }
+
+        return true;
     }
 
-    public static boolean move(String src, String dest) throws  Exception{
-        return false;
+    public static boolean move(String src, String dest) throws Exception {
+        //Files.copy(Pa, dest, StandardCopyOption.REPLACE_EXISTING);
+
+        return true;
     }
 
-    public static List<String> find(String path, String targetName) throws FileNotFoundException{
+    public static List<String> find(String path, String targetName) throws FileNotFoundException {
+        Reader reader = new FileReader(path);
+        StringBuilder sb = new StringBuilder();
+        List<String> result = null;
+
+        try {
+            while (reader.read() != -1) {
+                result.add(sb.append('\n').toString());
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static List<String> grep(String lines, String targetWord) {
+
         return null;
     }
 
-    public static List<String> grep(String lines, String targetWord){
+    public static Map<String, String> grepR(String path, String targetWord) {
+
         return null;
     }
-
-    public static Map<String, String> grepR(String path, String targetWord){
-        return null;
-    }
-
 }
