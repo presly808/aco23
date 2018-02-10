120027c61a5a57275256bae2d10d7546bdb232f6
ochernykh <0595887@gmail.com>
2018-02-10 10:55:34 +0200

    week4-data_structures-Oleh Chernykh

--- OlehChernykh/src/main/java/io/BashUtils.java
+++ OlehChernykh/src/main/java/io/BashUtils.java
@@ -11,39 +11,55 @@ import java.util.Map;
 public class BashUtils {
 
     public static String cat(String path) throws FileNotFoundException {
-        File folder = new File("/home/oleh/folder/");
-        File[] listOfFiles = folder.listFiles();
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
+        Reader reader = new FileReader(path);
+        StringBuilder sb = new StringBuilder();
+        char[] buffer = new char[1024];
+        int count;
+        try {
+            while ((count = reader.read(buffer)) != -1) {
+                sb.append(buffer, 0, count);
             }
+        }catch (IOException e){
+            e.printStackTrace();
         }
-        return listOfFiles.toString();
+        return sb.toString();
+
         //return Files.walk(Path.get(path)).filter(Files::isRegularFile).forEach(System.out::println);
     }
 
-    public static boolean writeInto(String path, String src, boolean append)
-            throws IOException {
-        try (Writer writer = new FileWriter(path,append)){
+    public static boolean writeInto(String path, String src, boolean append) throws IOException {
+        try (Writer writer = new FileWriter(path)){
             writer.write(src);
-            writer.flush();
+            if (append){
+                writer.flush();
+            }
+            writer.close();
         }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-
-        return false;
+        return true;
     }
 
     public static List<String> ls(String path) throws FileNotFoundException {
-        File root = new File( path );
-        File[] files = root.listFiles().;
-
-        return null;
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+        List<String> list = new ArrayList<>();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() || file.isDirectory()) {
+                    list.add(file.getName());
+                }
+            }
+            return list;
+        }
+        throw new FileNotFoundException() ;
     }
 
     public static boolean copy(String src, String dest) throws Exception {
-        return false;
+        if (src != null){
+            String buffer = cat(src);
+            writeInto(dest,buffer,true);
+            return true;
+        }
+        throw new Exception();
     }
 
     public static boolean move(String src, String dest) throws  Exception{
@@ -51,6 +67,7 @@ public class BashUtils {
     }
 
     public static List<String> find(String path, String targetName) throws FileNotFoundException{
+
         return null;
     }
 
