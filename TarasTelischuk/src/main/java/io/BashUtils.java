b271609fd2f135e4f71cf9a0879caafc7628f10f
telishuk <telishuk.t@gmail.com>
2018-02-11 11:32:27 +0200

    <IO><Telischuk>

--- TarasTelischuk/src/main/java/io/BashUtils.java
+++ TarasTelischuk/src/main/java/io/BashUtils.java
@@ -3,9 +3,11 @@ package io;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by serhii on 10.02.18.
@@ -52,33 +54,73 @@ public class BashUtils {
     }
 
     public static List<String> ls(String path) throws FileNotFoundException {
-        return Arrays.asList(System.getenv(path).split(":"));
+        if (!Files.exists(Paths.get(path))) {
+            throw new FileNotFoundException();
+        }
+        File file = new File(path);
+            if (file.isDirectory()) {
+               return Arrays.stream(file.listFiles()).map(File::getName).collect(Collectors.toList());
+
+        }
+        return null;
     }
 
     public static boolean copy(String src, String dest) throws Exception {
-        if (src != null){
-            String buff = cat(src);
-            return writeInto(dest, buff, true);
+       InputStream in = new FileInputStream(new File(src));
+       OutputStream out = new FileOutputStream(new File(dest));
+
+       try{
+           byte[] buff = new byte[1024];
+           int count ;
+           while((count = in.read(buff)) != -1 ){
+                out.write(buff, 0, count);
+                out.flush();
+            }
         }
-        return false;
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+        return true;
     }
 
     public static boolean move(String src, String dest) throws  Exception{
-        boolean res = copy(src, dest);
-        new File(src).deleteOnExit();
+        copy(src, dest);
+        boolean res;
+        File file = new File(src);
+        res = file.delete();
         return res;
     }
 
     public static List<String> find(String path, String targetName) throws FileNotFoundException{
-        return null;
+        if (!Files.exists(Paths.get(path))) {
+            throw new FileNotFoundException();
+        }
+        File file = new File(path);
+        return findR(file, targetName);
     }
 
     public static List<String> grep(String lines, String targetWord){
-        return null;
+        return Arrays.stream(lines.split("\n")).sorted().filter(line -> line.contains(targetWord)).collect(Collectors.toList());
     }
 
     public static Map<String, String> grepR(String path, String targetWord){
         return null;
+    }
+
+
+
+    private static List<String> findR(File file, String targetName){
+    List<String> list = new ArrayList<>();
+    Arrays.stream(file.listFiles()).forEachOrdered(f -> {
+        if (f.isDirectory()) {
+            list.addAll(findR(f, targetName));
+        } else {
+            if (f.getName().contains(targetName)) {
+                list.add(f.getName());
+            }
+        }
+        });
+        return list;
     }
 
 }
