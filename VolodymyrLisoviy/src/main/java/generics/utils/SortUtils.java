0e6aac03fb4807381193dbc48c1bd04a34db95b2
Foresstt <les.volodumur@gmail.com>
2018-02-11 10:29:06 +0200

    week5- io - Volodymyr Lisoviy - copy task

--- VolodymyrLisoviy/src/main/java/generics/utils/SortUtils.java
+++ VolodymyrLisoviy/src/main/java/generics/utils/SortUtils.java
@@ -1,21 +1,60 @@
 package generics.utils;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by serhii on 11.02.18.
  */
 public class SortUtils {
 
-    // todo implement and add generics
-    public static List mergeSort(List elements, Comparator comparator){
-        return null;
+    public static <T> List<T> mergeSort(List<T> elements, Comparator<T> comparator) {
+        int size = elements.size();
+        if (size == 1)
+            return elements;
+
+        int middle = size / 2;
+        List<T> left = mergeSort(elements.subList(0, middle), comparator);
+        List<T> right = mergeSort(elements.subList(middle, size), comparator);
+
+        return merge(left, right, comparator);
     }
 
 
-    public static int compare(Object o1, Object o2){
-        return 0;
+    public static <T extends Comparable<T>> int compare(T o1, T o2) {
+        return o1.compareTo(o2);
+    }
+
+    private static <T> List<T> merge(List<T> left, List<T> right, Comparator<T> comparator) {
+        ListIterator<T> leftItr = left.listIterator();
+        ListIterator<T> rightItr = right.listIterator();
+
+        List<T> sortedList = new ArrayList<>();
+
+
+        while (leftItr.hasNext() && rightItr.hasNext()) {
+            Optional<T> leftO = Optional.of(leftItr.next());
+            Optional<T> rightO = Optional.of(rightItr.next());
+
+            if (comparator.compare(leftO.get(), rightO.get()) < 0) {
+                // left >= right
+                sortedList.add(leftO.get());
+                rightItr.previous();
+            } else {
+                // left < right
+                sortedList.add(rightO.get());
+                leftItr.previous();
+            }
+        }
+
+        while (leftItr.hasNext()) {
+            sortedList.add(leftItr.next());
+        }
+
+        while (rightItr.hasNext()) {
+            sortedList.add(rightItr.next());
+        }
+
+        return sortedList;
     }
 
 
