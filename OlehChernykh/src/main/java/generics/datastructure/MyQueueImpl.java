4e8b172ce96622c7647208a8dab10a51f1b71205
ochernykh <0595887@gmail.com>
2018-02-11 10:39:32 +0200

    copy tasks js, generics

--- OlehChernykh/src/main/java/generics/datastructure/MyQueueImpl.java
+++ OlehChernykh/src/main/java/generics/datastructure/MyQueueImpl.java
@@ -6,23 +6,57 @@ import java.util.Iterator;
  * Created by serhii on 11.02.18.
  */
 public class MyQueueImpl<T> implements MyQueue<T> {
+
+    private T[] mas = (T[]) new Object[10];
+    private int head;
+    private int tail;
+    private int size;
+
     @Override
-    public boolean queue(T el) throws NoFreeSpaceException {
-        return false;
+    public boolean enqueue(T el) throws NoFreeSpaceException {
+        if (size > mas.length) {
+            throw new NoFreeSpaceException();
+        }
+        mas[tail++] = el;
+        size++;
+        return true;
     }
 
     @Override
-    public T enqueue() throws NoElementsException {
-        return null;
+    public T dequeue() throws NoElementsException {
+        if(size == 0){
+            throw  new NoElementsException();
+        }
+        T ret = mas[head++];
+        size--;
+        return ret;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new MyQueueIterator();
     }
+
+    private class MyQueueIterator implements Iterator<T> {
+
+        int curr = head;
+
+        @Override
+        public boolean hasNext() {
+            return curr < tail;
+        }
+
+        @Override
+        public T next() {
+            T ret = mas[curr++];
+            return ret;
+        }
+    }
+
+
 }
--- OlehChernykh/src/main/java/generics/datastructure/NoFreeSpaceException.java
+++ OlehChernykh/src/main/java/generics/datastructure/NoFreeSpaceException.java
@@ -4,4 +4,5 @@ package generics.datastructure;
  * Created by serhii on 11.02.18.
  */
 public class NoFreeSpaceException extends Exception {
+
 }
