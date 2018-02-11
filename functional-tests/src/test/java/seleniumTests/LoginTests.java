aa06494e1fa647bfa90dfa18648bec7ef189c11a
presly808 <presly808@gmail.com>
2018-02-11 14:50:58 +0200

    upgraded gradle

--- functional-tests/src/test/java/seleniumTests/LoginTests.java
+++ functional-tests/src/test/java/seleniumTests/LoginTests.java
@@ -7,7 +7,6 @@ public class LoginTests extends Fixture {
     @Test
     public void test1() {
         System.out.println("hello");
-        System.out.println("YRA");
         driver.navigate().to("https://www.google.com.ua/");
     }
 }
