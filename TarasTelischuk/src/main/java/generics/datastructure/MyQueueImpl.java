7e8f6f9b81014c7452ba76c1849216a559a2a1c8
telishuk <telishuk.t@gmail.com>
2018-02-13 11:13:43 +0200

    <generics><Telischuk>

---
+++ TarasTelischuk/src/main/java/generics/datastructure/MyQueueImpl.java
@@ -0,0 +1,46 @@
+package generics.datastructure;
+
+import java.util.Iterator;
+
+/**
+ * Created by serhii on 11.02.18.
+ */
+public class MyQueueImpl<T> implements MyQueue<T> {
+
+    private T [] mas = (T[]) new Object[900];
+    private int head;
+    private int tail;
+    private int size;
+
+
+    @Override
+    public boolean enqueue(T el) throws NoFreeSpaceException {
+        if (size >= mas.length){
+            throw new NoFreeSpaceException();
+        }
+        mas[tail] = el;
+        size++;
+        return false;
+    }
+
+    @Override
+    public T dequeue() throws NoElementsException {
+        if (size == 0) {
+            throw new NoElementsException();
+        }
+        T ret = mas[head];
+        head++;
+        size--;
+        return ret;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
