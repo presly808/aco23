29d6dd3b6f4f88bfc7360074bbf1c6e64c59768a
Mihal <miandmih@gmail.com>
2018-02-15 19:52:29 +0200

    Week5 - generics -  CodacyFix

--- AndreyMihailenko/src/main/java/generics/datastructure/MyQueue.java
+++ AndreyMihailenko/src/main/java/generics/datastructure/MyQueue.java
@@ -1,8 +1,5 @@
 package generics.datastructure;
 
-/**
- * Created by serhii on 11.02.18.
- */
 public interface MyQueue<T> extends Iterable<T> {
 
 
--- AndreyMihailenko/src/main/java/generics/datastructure/NoElementsException.java
+++ AndreyMihailenko/src/main/java/generics/datastructure/NoElementsException.java
@@ -1,7 +1,4 @@
 package generics.datastructure;
 
-/**
- * Created by serhii on 11.02.18.
- */
 public class NoElementsException extends Exception {
 }
