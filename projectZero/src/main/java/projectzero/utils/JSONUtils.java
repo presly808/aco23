f5572b8942435a53743e41f23f57522b0974f065
Mihal <miandmih@gmail.com>
2018-02-24 21:29:14 +0200

    JSONUtils

--- projectZero/src/main/java/projectzero/utils/JSONUtils.java
+++ projectZero/src/main/java/projectzero/utils/JSONUtils.java
@@ -1,59 +1,90 @@
 package projectzero.utils;
 
 import com.google.gson.Gson;
-import org.openqa.selenium.json.Json;
-import projectzero.model.Employee;
 import projectzero.model.User;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-
 public class JSONUtils {
 
-    /*JSONUtils
-    * - void writeIntoFile (path, user)
-    * - User findUser ()
-    * - List<User> readFromFile (path)
-    * -
-    * */
-
-    private User user;
     private static Gson gson = new Gson();
-    private File employeesFile = new File("Employees.json");
-    private File usersFile = new File("Users.json");
 
-    public static void writeIntoFile (String path, User user){
+    /**
+     * Add User to Json file
+     *
+     * @param path - path of Json file
+     * @param user - adding user
+     * @throws IOException
+     */
+    public static void addUser(String path, User user) throws IOException {
+        List<User> users = readFromFile(path);
+        users.add(user);
+        writeListIntoFile(path, users);
+    }
+
+    /**
+     * Write a list of users into Json file
+     *
+     * @param path  - path of Json file
+     * @param users - list af adding users
+     */
+    private static void writeListIntoFile(String path, List<User> users) {
         File file = new File(path);
-        if (file.exists() && file.canWrite()){
-            try (Writer writer = new FileWriter(file, true)) {
-                writer.write(gson.toJson(user));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try (Writer writer = new FileWriter(file, false)) {
+            writer.write(gson.toJson(users));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public User findUser (String path){
+    /**
+     * @param path - path of Json file
+     * @param email - incoming email
+     * @return - User with incoming email
+     * @throws IOException
+     */
+    public static User findUserByEmail(String path, String email) throws IOException {
+        List<User> users = readFromFile(path);
         return null;
     }
 
-    public List<User> readFromFile (String path) throws IOException {
-        String jString = new String(Files.readAllBytes(Paths.get(path)));
-        User[] users = gson.fromJson(jString, User[].class);
-        List<User> list = new ArrayList<>();
-        Collections.addAll(list, users);
-        return list;
+    /**
+     * Remove user from Json file
+     *
+     * @param path - path of Json file
+     * @param user - user which will removed
+     * @throws IOException
+     */
+    public void removeUser(String path, User user) throws IOException {
+        List<User> users = readFromFile(path);
+        users.remove(user);
+        writeListIntoFile(path, users);
     }
 
+    /**
+     * Reading from Json file
+     *
+     * @param path - path of Json file
+     * @return list of users in Json file
+     * @throws IOException
+     */
+    public static List<User> readFromFile(String path) throws IOException {
+        File file = new File(path);
+        List<User> list = new ArrayList<>();
+        if (file.length() != 0) {
+            String jString = new String(Files.readAllBytes(Paths.get(path)));
+            User[] users = gson.fromJson(jString, User[].class);
+            Collections.addAll(list, users);
 
-
-
+            return list;
+        }
+        return list;
+    }
 
 }
