d5354f7b32a63ca0bd53a0089be2a00f4a743c8f
yuriykolomiyets <juriy121@gmail.com>
2018-02-24 10:37:25 +0200

    Week7 - model

--- TeamOne/src/main/java/model/User.java
+++ TeamOne/src/main/java/model/User.java
@@ -2,5 +2,45 @@ package model;
 
 public class User {
 
+    private int id;
+    private String email;
+    private String pass;
 
+    public User() {
+    }
+
+    public User(int id, String email, String pass) {
+        this.id = id;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public User(int id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
