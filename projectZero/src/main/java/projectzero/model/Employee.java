467513d3c73e788c29305f9860cb1e6fbe54b7af
Vitalii Laputko <vitalii.l@sbtech.com>
2018-02-24 10:25:06 +0200

    added model

--- projectZero/src/main/java/projectzero/model/Employee.java
+++ projectZero/src/main/java/projectzero/model/Employee.java
@@ -1,4 +1,49 @@
 package projectzero.model;
 
 public class Employee {
+
+    private final int id;
+    private String name;
+    private int salary;
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Employee{" +
+                "id=" + id + '}';
+    }
 }
--- projectZero/src/main/java/projectzero/model/User.java
+++ projectZero/src/main/java/projectzero/model/User.java
@@ -1,4 +1,48 @@
 package projectzero.model;
 
 public class User {
+
+    private final int email;
+    private String logg;
+    private String pass;
+
+    public User(int email, String logg, String pass) {
+        this.email = email;
+        this.logg = logg;
+        this.pass = pass;
+    }
+
+    public User(int email) {
+        this.email = email;
+    }
+
+
+    public int getEmail() {
+        return email;
+    }
+
+    public String getLogg() {
+        return logg;
+    }
+
+    public void setLogg(String logg) {
+        this.logg = logg;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "email=" + email +
+                ", logg='" + logg + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
 }
