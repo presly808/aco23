89284a0e54745c21a1860df3ab82033fcc07cf24
Mihal <miandmih@gmail.com>
2018-03-02 13:39:25 +0200

    writing some JavaDoc's

--- projectZero/src/main/java/projectzero/utils/LogUtils.java
+++ projectZero/src/main/java/projectzero/utils/LogUtils.java
@@ -10,6 +10,10 @@ public class LogUtils {
         PropertyConfigurator.configure(LogUtils.class.getResource("/log4j.properties"));
     }
 
+    /**
+     * @param classForLogging - class which will logged
+     * @return Logger for current class
+     */
     public static Logger getLogger(Class classForLogging) {
         return Logger.getLogger(classForLogging);
     }
