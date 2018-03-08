b007302f655e26f9948c123751c335d63793a33e
Foresstt <les.volodumur@gmail.com>
2018-03-08 18:01:01 +0200

    projectzero - add app.properties, mail.properties

--- projectZero/src/main/java/projectzero/Main.java
+++ projectZero/src/main/java/projectzero/Main.java
@@ -1,16 +1,49 @@
 package projectzero;
 
 import projectzero.server.Server;
+import projectzero.utils.EmailUtils;
+import projectzero.utils.LogUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 // todo describe in read me how to run server
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Run with 2 args");
+        if (args.length < 1) {
+            System.out.println("Enter path to a config file as the 1st arg");
             System.exit(-1);
         }
-        // todo name args
-        new Server(9817, args[0], args[1]);
+
+        Properties appProperties = new Properties();
+        try (InputStream io = new FileInputStream(args[0])) {
+            appProperties.load(io);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        String pathToLog4jProperties = appProperties.getProperty("pathToLog4jProperties");
+        LogUtils.configLogger(pathToLog4jProperties);
+
+
+        String pathToMailProperties = appProperties.getProperty("pathToMailProperties");
+
+        Properties mailProperties = new Properties();
+        try (InputStream io = new FileInputStream(pathToMailProperties)) {
+            mailProperties.load(io);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        EmailUtils.setProps(mailProperties);
+
+        String pathToUsersJSON = appProperties.getProperty("pathToUsersJSON");
+        String pathToOrdersJSON = appProperties.getProperty("pathToOrdersJSON");
+
+        new Server(9817, pathToUsersJSON, pathToOrdersJSON);
     }
 }
